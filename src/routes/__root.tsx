03e69b1bb0bb4c777fa2bd513491a2c257fc36ae
lizbermeo88-web <lizbermeo88@gmail.com>
2026-09-23 21:57:48 +0200

    Update __root.tsx
    Arreglo de código suelto

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -38,9 +38,9 @@ function NotFoundComponent() {
 function ErrorComponent({ error, reset }: { error: Error; reset: () => void }) {
   console.error(error);
   const router = useRouter();
-  useEffect(() => {
-    reportLovableError(error, { boundary: "tanstack_root_error_component" });
-  }, [error]);
+ useEffect(() => {
+  console.error(error);
+}, [error]);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-background px-4">
